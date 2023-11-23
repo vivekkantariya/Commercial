ede4bc387c99e302a773a091d5773fd5ecc35d7c
Vivek <103636807+vivekkantariya@users.noreply.github.com>
2023-11-23 11:51:29 +0530

    Fixed

--- Application/myapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import BookFree, ContactSubmission
-
-# Register your models here.
-admin.site.register(BookFree)
-admin.site.register(ContactSubmission)
--- Application/sql_queries.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `admin_login`
-MODIFY COLUMN `username` varchar(50) NOT NULL UNIQUE;
